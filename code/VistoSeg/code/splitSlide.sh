ea3b1af9abf957d210b1e3d22c8af6c9954e33d7
heenadivecha <heenadivecha@gmail.com>
2023-08-29 14:09:43 -0400

    Added scripts to batch run splitSlide function from VistoSeg  pipeline

---
+++ code/VistoSeg/code/splitSlide.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -pe local 8
+#$ -o /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code/logs/$TASK_ID_splitSlide.txt
+#$ -e /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code/logs/$TASK_ID_splitSlide.txt
+#$ -m e
+#$ -M heenadivecha@gmail.com
+#$ -t 1-8
+#$ -tc 4
+
+
+echo "**** Job starts ****"
+date
+
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+echo "****"
+echo "Sample id: $(cat /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code/logs/$TASK_ID_splitSlide.txt | awk '{print $NF}' | awk "NR==${SGE_TASK_ID}")"
+echo "****"
+
+
+## load MATLAB
+module load matlab/R2019a
+
+## Load toolbox for VistoSeg
+toolbox='/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code'
+
+## Read inputs from splitSlide.txt file
+fname=$(awk 'BEGIN {FS="\t"} {print $1}' refineVNS_list.txt | awk "NR==${SGE_TASK_ID}")
+
+## Run splitSlide function
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide('$fname',0,0,0,0)"
+
+echo "**** Job ends ****"
+date
