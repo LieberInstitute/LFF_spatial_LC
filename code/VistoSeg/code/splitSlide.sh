670de436375ec920827b41a8da139fe0ce3d5c4e
heenadivecha <heenadivecha@gmail.com>
2023-09-06 13:28:25 -0400

    Version controling .sh script for splitSlide function

--- code/VistoSeg/code/splitSlide.sh
+++ code/VistoSeg/code/splitSlide.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -l mem_free=30G,h_vmem=30G,h_fsize=100G
 #$ -pe local 8
 #$ -o /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code/logs/$TASK_ID_splitSlide.txt
 #$ -e /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code/logs/$TASK_ID_splitSlide.txt
 #$ -m e
 #$ -M heenadivecha@gmail.com
-#$ -t 1-8
+#$ -t 4-8
 #$ -tc 4
 
 
@@ -21,7 +21,7 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: $(cat /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code/logs/$TASK_ID_splitSlide.txt | awk '{print $NF}' | awk "NR==${SGE_TASK_ID}")"
+echo "Sample id: $(cat /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg/code/splitSlide.txt | awk '{print $NF}' | awk "NR==${SGE_TASK_ID}")"
 echo "****"
 
 
