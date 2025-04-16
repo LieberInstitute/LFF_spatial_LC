2951f151e273d41eaf50db50398ac81a2cfd52b7
Madhavi Tippani <madhavitippani28@gmail.com>
2025-04-15 23:16:32 -0400

    calculate tissue NM metric with and without background subtraction

--- code/VistoSeg/code/NMscore.m
+++ code/VistoSeg/code/NMscore.m
@@ -14,7 +14,7 @@ if size(naIndices, 1)> 0 , disp([naIndices, 'donot have annotations']); end
 files = dir(fullfile(pwd, '/raw-data/Images/*1.tif'));
 myfiles = files(cellfun(@(x) length(x) == 17, {files.name}));
 
-for i= 8:numel(myfiles)
+for i= 1:numel(myfiles)
 fname = myfiles(i).name(1:end-4);
 disp(fname);
 img = imread([pwd, '/raw-data/Images/',fname,'.tif']);
@@ -117,4 +117,77 @@ writetable(Mdata, fullfile(pwd,'processed-data/Images/NMscore.csv'));
 
 
 
+
+%%%%%%%
+% compare mean nm intensities before and after BG subtraction
+
+for i= 1:numel(myfiles)
+fname = myfiles(i).name(1:end-4);
+disp(fname);
+img = imread([pwd, '/raw-data/Images/',fname,'.tif']);
+
+NMseg_dir = fullfile(pwd, '/processed-data/Images/NMseg/');
+load([NMseg_dir, fname, 'NMseg_clean.mat'])
+
+img1 = mat2gray(rgb2gray(img));
+BW = img1 < 0.9;     
+
+% bernie annotation based NMScore
+df = tb(strcmp(tb.sample_id,fname) & strcmp(tb.section, 'section_1'), :);
+x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
+k = convhull(y,x);
+Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
+BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
+img1(~BmaskImg) = 0;
+
+BG_mask = img1; NM_mask = img1;
+NM_mask(~NM) = 0; BG_mask(NM) = 0;
+nm = NM_mask(NM_mask>0);
+
+% Pi = sum(nm);
+% maxNM = max(nm);
+% Mt = size(nm,1)*maxNM;
+% NMscore1 = Pi/Mt;
+% 
+% Mdata.NMscore1(Mdata.sample_id == fname) = NMscore1;
+
+HE = mean(BG_mask(:));
+Pi = sum(nm-HE);
+maxNM = max(nm-HE);
+Mt = size(nm,1)*maxNM;
+BGNMscore1 = Pi/Mt;
+%temp1 = sum(nm-HE < 0);
+
+%Mdata.BGNMscore1(Mdata.sample_id == fname) = BGNMscore1;
+Mdata.tempsize1(Mdata.sample_id == fname) = temp1;
+
+img1 = mat2gray(rgb2gray(img));
+df = tb(strcmp(tb.sample_id,fname) & strcmp(tb.section, 'section_2'), :);
+x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
+k = convhull(y,x);
+Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
+BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
+img1(~BmaskImg) = 0;
+
+BG_mask = img1; NM_mask = img1;
+NM_mask(~NM) = 0; BG_mask(NM) = 0;
+nm = NM_mask(NM_mask>0);
+
+%Pi = sum(nm);
+%maxNM = max(nm);
+%Mt = size(nm,1)*maxNM;
+%NMscore2 = Pi/Mt;
+
+%Mdata.NMscore2(Mdata.sample_id == fname) = NMscore2;
+
+HE = mean(BG_mask(:));
+Pi = sum(nm-HE);
+maxNM = max(nm-HE);
+Mt = size(nm,1)*maxNM;
+BGNMscore2 = Pi/Mt;
+%temp2 = sum(nm-HE < 0);
+
+%Mdata.BGNMscore2(Mdata.sample_id == fname) = BGNMscore2;
+Mdata.tempsize2(Mdata.sample_id == fname) = temp2;
+end
 
