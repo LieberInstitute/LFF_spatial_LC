4ae17ca5683eea24a2776abc171dbba7bac4a5d5
Madhavi Tippani <madhavitippani28@gmail.com>
2025-04-16 09:08:51 -0400

    adapt bernie's code to combine new spot metrics to spe

--- code/VistoSeg/code/compile_BGspotMetrics.R
+++ code/VistoSeg/code/compile_BGspotMetrics.R
@@ -36,3 +36,25 @@ segmentations <-
   }, segmentations_list[lengths(segmentations_list) > 0])
 
 
+  library(S4Vectors)
+  lc3 <- readRDS(here("processed-data/06-countsOnly_noImgData_QCed_SPE_split_to_tissSections.RDS"))
+  louvs <- readRDS(here("processed-data/07_featureSelection_dimred_harmony_clustering/04-Initial_louvLeid_res0.5-1-2_clusterings.RDS"))
+  louvs <- louvs[["HARMONYlmbna_HDG_SVG_2575"]][,c("rn","snnHARMONYlmbna_HDG_SVG_2575_louv_res1")]
+  colnames(louvs)[2] <- "clusid"
+  anno <- read.table(here("processed-data/08_validitycheck_25hdg75svg_louv1/10-25hdg75svg_louv1_annots.txt"),header=T)
+  louvs <- merge(louvs,anno,by="clusid")
+  louvs <- DataFrame(louvs,row.names=louvs$rn)[colnames(lc3),]
+  colLabels(lc3) <- louvs$anno 
+  unique_spd_values <- unique(lc3$label)
+
+
+  ## Add the information
+  segmentation_match <- match(lc3$original_spot_id, segmentations$key)
+  segmentation_info <-
+    segmentations[segmentation_match, -which(
+      colnames(segmentations) %in% c("barcode", "tissue", "row", "col", "imagerow", "imagecol", "key")
+    )]
+  colData(lc3) <- cbind(colData(lc3), segmentation_info)
+
+  # Convert colData to dataframe
+  df <- as.data.frame(colData(lc3))
