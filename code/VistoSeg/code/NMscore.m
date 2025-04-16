2ca6bfecdd14ac54f33d707561d7d3808d7f4de7
Madhavi Tippani <madhavitippani28@gmail.com>
2025-04-16 00:39:26 -0400

    extract tissue NM metric for LC cluster based BG subtracted samples

--- code/VistoSeg/code/NMscore.m
+++ code/VistoSeg/code/NMscore.m
@@ -14,6 +14,7 @@ if size(naIndices, 1)> 0 , disp([naIndices, 'donot have annotations']); end
 files = dir(fullfile(pwd, '/raw-data/Images/*1.tif'));
 myfiles = files(cellfun(@(x) length(x) == 17, {files.name}));
 
+%% bernie annotation based BG 
 for i= 1:numel(myfiles)
 fname = myfiles(i).name(1:end-4);
 disp(fname);
@@ -200,7 +201,7 @@ naIdx = strcmp(clus.section, 'NA');
 naIndices = find(naIdx);
 if size(naIndices, 1)> 0 , disp([naIndices, 'donot have clusters']); end
 
-for i= 1:numel(myfiles)
+for i= 39:numel(myfiles)
 fname = myfiles(i).name(1:end-4);
 disp(fname);
 img = imread([pwd, '/raw-data/Images/',fname,'.tif']);
@@ -242,3 +243,64 @@ HE = mean(BG_mask(:));
 temp2 = sum(nm-HE < 0);
 Mdata.LCtempsize2(Mdata.sample_id == fname) = temp2;
 end
+
+
+%%
+% LC cluster based BG
+for i= 1:numel(myfiles)
+      if ismember(i, [7, 11, 18])
+        continue
+      end
+fname = myfiles(i).name(1:end-4);
+disp(fname);
+img = imread([pwd, '/raw-data/Images/',fname,'.tif']);
+
+NMseg_dir = fullfile(pwd, '/processed-data/Images/NMseg/');
+load([NMseg_dir, fname, 'NMseg_clean.mat'])
+
+img1 = imcomplement(mat2gray(rgb2gray(img)));
+BW = img1 < 0.1;     
+
+df = clus(strcmp(clus.sample_id,fname) & strcmp(clus.section, 's1'), :);
+x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
+k = convhull(y,x);
+Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
+BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
+img1(~Bmask) = 0;
+BG_mask = img1; NM_mask = img1;
+NM_mask(~NM) = 0; BG_mask(NM) = 0;
+
+HE = mean(BG_mask(:));
+nm = NM_mask(NM_mask>0);
+Pi = sum(nm-HE);
+maxNM = max(nm-HE);
+Mt = size(nm,1)*maxNM;
+LCBGNMscore1 = Pi/Mt;
+
+Mdata.LCBGNMscore1(Mdata.sample_id == fname) = LCBGNMscore1;
+
+img1 = imcomplement(mat2gray(rgb2gray(img)));
+BW = img1 < 0.1;     
+
+df = clus(strcmp(clus.sample_id,fname) & strcmp(clus.section, 's2'), :);
+x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
+k = convhull(y,x);
+Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
+BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
+img1(~Bmask) = 0;
+BG_mask = img1; NM_mask = img1;
+NM_mask(~NM) = 0; BG_mask(NM) = 0;
+
+HE = mean(BG_mask(:));
+nm = NM_mask(NM_mask>0);
+Pi = sum(nm-HE);
+maxNM = max(nm-HE);
+Mt = size(nm,1)*maxNM;
+LCBGNMscore2 = Pi/Mt;
+
+Mdata.LCBGNMscore2(Mdata.sample_id == fname) = LCBGNMscore2;
+
+disp(i)
+end
+
+writetable(Mdata, fullfile(pwd,'processed-data/Images/NMscore_LCBG.csv'));
