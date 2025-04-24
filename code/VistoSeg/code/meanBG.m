0f6c76a5a96f15802bf9b21b5a034864b5a0dc1a
Madhavi Tippani <madhavitippani28@gmail.com>
2025-04-24 11:40:06 -0400

    meanBG.m

--- code/VistoSeg/code/meanBG.m
+++ code/VistoSeg/code/meanBG.m
@@ -1,11 +1,11 @@
 cd '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC'
 
 %% load LC cluster, tissue annotations and bounding boxes
-annot = fullfile(pwd, '/processed-data/Images/06c-tissueOverlapping_spots_with_fullres-pixel-row-col-coords_and_section_annotations.txt');
-tb = readtable(annot);
-naIdx = strcmp(tb.section, 'NA');
-naIndices = find(naIdx);
-if size(naIndices, 1)> 0 , disp([naIndices, 'donot have annotations']); end
+% annot = fullfile(pwd, '/processed-data/Images/06c-tissueOverlapping_spots_with_fullres-pixel-row-col-coords_and_section_annotations.txt');
+% tb = readtable(annot);
+% naIdx = strcmp(tb.section, 'NA');
+% naIndices = find(naIdx);
+% if size(naIndices, 1)> 0 , disp([naIndices, 'donot have annotations']); end
 
 cluster = fullfile(pwd, '/processed-data/LC_spotAndPixel_coords_25hdg75svg_louv1.txt');
 clus = readtable(cluster);
@@ -35,116 +35,116 @@ BW = img1 < 0.9;
      
 img1i = imcomplement(img1);
 BWi = img1i > 0.1;
-%% mean BG intensity from entire tissue section
-%section 1
-df = tb(strcmp(tb.sample_id,fname) & strcmp(tb.section, 'section_1'), :);
-x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
-k = convhull(y,x);
-Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
-
-BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
-img1(~BmaskImg) = 0;
-BG_mask = img1; 
-BG_mask(NM) = 0;
-BGmask_tis1 = BG_mask;
-tis1 = mean(BG_mask(:));
-
-BmaskImg = bsxfun(@times, BWi, cast(Bmask, class(BWi)));
-img1i(~BmaskImg) = 0;
-BG_mask = img1i; 
-BG_mask(NM) = 0;
-BGmask_tis1i = BG_mask;
-tis1i = mean(BG_mask(:));
-
-%section 2
-img1 = mat2gray(rgb2gray(img));
-img1i = imcomplement(img1);
-df = tb(strcmp(tb.sample_id,fname) & strcmp(tb.section, 'section_2'), :);
-x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
-k = convhull(y,x);
-Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
-
-BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
-img1(~BmaskImg) = 0;
-BG_mask = img1; 
-BG_mask(NM) = 0;
-BGmask_tis2 = BG_mask;
-tis2 = mean(BG_mask(:));
-
-BmaskImg = bsxfun(@times, BWi, cast(Bmask, class(BWi)));
-img1i(~BmaskImg) = 0;
-BG_mask = img1i; 
-BG_mask(NM) = 0;
-BGmask_tis2i = BG_mask;
-tis2i = mean(BG_mask(:));
-
-%% mean BG intensity from LC bounding box
-%section1
-img1 = mat2gray(rgb2gray(img));
-img1i = imcomplement(img1);
-roi = Mdata.BB1{Mdata.sample_id == fname};  
-x = roi(1); y = roi(2); w = roi(3); h = roi(4);
-mask = false(size(img,1), size(img,2));
-mask(y:y+h-1, x:x+w-1) = true;  % Set ROI region to true
-
-maskImg = bsxfun(@times, BW, cast(mask, class(BW)));
-img1(~maskImg) = 0;
-BG_mask = img1; 
-BG_mask(NM) = 0;
-BGmask_BB1 = BG_mask;
-BB1 = mean(BG_mask(:));
-
-maskImg = bsxfun(@times, BWi, cast(mask, class(BWi)));
-img1i(~maskImg) = 0;
-BG_mask = img1i; 
-BG_mask(NM) = 0;
-BGmask_BB1i = BG_mask;
-BB1i = mean(BG_mask(:));
-
-%section2
-img1 = mat2gray(rgb2gray(img));
-img1i = imcomplement(img1);
-roi = Mdata.BB2{Mdata.sample_id == fname};  
-x = roi(1); y = roi(2); w = roi(3); h = roi(4);
-mask = false(size(img,1), size(img,2));
-mask(y:y+h-1, x:x+w-1) = true;  % Set ROI region to true
-
-maskImg = bsxfun(@times, BW, cast(mask, class(BW)));
-img1(~maskImg) = 0;
-BG_mask = img1; 
-BG_mask(NM) = 0;
-BGmask_BB2 = BG_mask;
-BB2 = mean(BG_mask(:));
-
-maskImg = bsxfun(@times, BWi, cast(mask, class(BWi)));
-img1i(~maskImg) = 0;
-BG_mask = img1i; 
-BG_mask(NM) = 0;
-BGmask_BB2i = BG_mask;
-BB2i = mean(BG_mask(:));
-
-%% mean BG intensity from LC cluster
-%section 1
-img1 = mat2gray(rgb2gray(img));
-img1i = imcomplement(img1);
-df = clus(strcmp(clus.sample_id,fname) & strcmp(clus.section, 's1'), :);
-x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
-k = convhull(y,x);
-Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
-
-BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
-img1(~BmaskImg) = 0;
-BG_mask = img1; 
-BG_mask(NM) = 0;
-BGmask_LC1 = BG_mask;
-LC1 = mean(BG_mask(:));
-
-BmaskImg = bsxfun(@times, BWi, cast(Bmask, class(BWi)));
-img1i(~BmaskImg) = 0;
-BG_mask = img1i; 
-BG_mask(NM) = 0;
-BGmask_LC1i = BG_mask;
-LC1i = mean(BG_mask(:));
+% %% mean BG intensity from entire tissue section
+% %section 1
+% df = tb(strcmp(tb.sample_id,fname) & strcmp(tb.section, 'section_1'), :);
+% x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
+% k = convhull(y,x);
+% Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
+% 
+% BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
+% img1(~BmaskImg) = 0;
+% BG_mask = img1; 
+% BG_mask(NM) = 0;
+% BGmask_tis1 = BG_mask;
+% tis1 = mean(BG_mask(:));
+% 
+% BmaskImg = bsxfun(@times, BWi, cast(Bmask, class(BWi)));
+% img1i(~BmaskImg) = 0;
+% BG_mask = img1i; 
+% BG_mask(NM) = 0;
+% BGmask_tis1i = BG_mask;
+% tis1i = mean(BG_mask(:));
+% 
+% %section 2
+% img1 = mat2gray(rgb2gray(img));
+% img1i = imcomplement(img1);
+% df = tb(strcmp(tb.sample_id,fname) & strcmp(tb.section, 'section_2'), :);
+% x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
+% k = convhull(y,x);
+% Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
+% 
+% BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
+% img1(~BmaskImg) = 0;
+% BG_mask = img1; 
+% BG_mask(NM) = 0;
+% BGmask_tis2 = BG_mask;
+% tis2 = mean(BG_mask(:));
+% 
+% BmaskImg = bsxfun(@times, BWi, cast(Bmask, class(BWi)));
+% img1i(~BmaskImg) = 0;
+% BG_mask = img1i; 
+% BG_mask(NM) = 0;
+% BGmask_tis2i = BG_mask;
+% tis2i = mean(BG_mask(:));
+% 
+% %% mean BG intensity from LC bounding box
+% %section1
+% img1 = mat2gray(rgb2gray(img));
+% img1i = imcomplement(img1);
+% roi = Mdata.BB1{Mdata.sample_id == fname};  
+% x = roi(1); y = roi(2); w = roi(3); h = roi(4);
+% mask = false(size(img,1), size(img,2));
+% mask(y:y+h-1, x:x+w-1) = true;  % Set ROI region to true
+% 
+% maskImg = bsxfun(@times, BW, cast(mask, class(BW)));
+% img1(~maskImg) = 0;
+% BG_mask = img1; 
+% BG_mask(NM) = 0;
+% BGmask_BB1 = BG_mask;
+% BB1 = mean(BG_mask(:));
+% 
+% maskImg = bsxfun(@times, BWi, cast(mask, class(BWi)));
+% img1i(~maskImg) = 0;
+% BG_mask = img1i; 
+% BG_mask(NM) = 0;
+% BGmask_BB1i = BG_mask;
+% BB1i = mean(BG_mask(:));
+% 
+% %section2
+% img1 = mat2gray(rgb2gray(img));
+% img1i = imcomplement(img1);
+% roi = Mdata.BB2{Mdata.sample_id == fname};  
+% x = roi(1); y = roi(2); w = roi(3); h = roi(4);
+% mask = false(size(img,1), size(img,2));
+% mask(y:y+h-1, x:x+w-1) = true;  % Set ROI region to true
+% 
+% maskImg = bsxfun(@times, BW, cast(mask, class(BW)));
+% img1(~maskImg) = 0;
+% BG_mask = img1; 
+% BG_mask(NM) = 0;
+% BGmask_BB2 = BG_mask;
+% BB2 = mean(BG_mask(:));
+% 
+% maskImg = bsxfun(@times, BWi, cast(mask, class(BWi)));
+% img1i(~maskImg) = 0;
+% BG_mask = img1i; 
+% BG_mask(NM) = 0;
+% BGmask_BB2i = BG_mask;
+% BB2i = mean(BG_mask(:));
+% 
+% %% mean BG intensity from LC cluster
+% %section 1
+% img1 = mat2gray(rgb2gray(img));
+% img1i = imcomplement(img1);
+% df = clus(strcmp(clus.sample_id,fname) & strcmp(clus.section, 's1'), :);
+% x = df.pxl_col_in_fullres; y = df.pxl_row_in_fullres;
+% k = convhull(y,x);
+% Bmask = poly2mask(x(k), y(k), size(BW, 1), size(BW, 2)); % Black out everything outside the polygon
+% 
+% BmaskImg = bsxfun(@times, BW, cast(Bmask, class(BW)));
+% img1(~BmaskImg) = 0;
+% BG_mask = img1; 
+% BG_mask(NM) = 0;
+% BGmask_LC1 = BG_mask;
+% LC1 = mean(BG_mask(:));
+% 
+% BmaskImg = bsxfun(@times, BWi, cast(Bmask, class(BWi)));
+% img1i(~BmaskImg) = 0;
+% BG_mask = img1i; 
+% BG_mask(NM) = 0;
+% BGmask_LC1i = BG_mask;
+% LC1i = mean(BG_mask(:));
 
 %section2
 img1 = mat2gray(rgb2gray(img));
