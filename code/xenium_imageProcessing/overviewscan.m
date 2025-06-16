13b6b51ce200e72a1a104e2ce9c3fbae88d32113
Madhavi Tippani <madhavitippani28@gmail.com>
2025-06-16 12:20:22 -0400

    extract x,y locations for each fov and then extract the min and max x,y locations

--- code/xenium_imageProcessing/overviewscan.m
+++ code/xenium_imageProcessing/overviewscan.m
@@ -3,3 +3,25 @@ jsonData = jsondecode(fileread('/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/L
 % Extract FOV locations
 fovs = jsonData.fov_locations;
 fovNames = fieldnames(fovs);
+
+
+% Preallocate
+xVals = zeros(length(fovNames), 1);
+yVals = zeros(length(fovNames), 1);
+widths = zeros(length(fovNames), 1);
+heights = zeros(length(fovNames), 1);
+
+% Loop through FOVs
+for i = 1:length(fovNames)
+    fov = fovs.(fovNames{i});
+    xVals(i) = fov.x;
+    yVals(i) = fov.y;
+    widths(i) = fov.width;
+    heights(i) = fov.height;
+end
+
+% Compute bounding box from x, y and their widths/heights
+x_min = min(xVals);
+x_max = max(xVals + widths);
+y_min = min(yVals);
+y_max = max(yVals + heights);
