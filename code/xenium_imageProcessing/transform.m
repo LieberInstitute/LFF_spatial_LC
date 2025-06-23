d505cc2c57f4658f08621880a3041764a65248a6
Madhavi Tippani <madhavitippani28@gmail.com>
2025-06-23 11:52:24 -0400

    code to transform He image to align with DAPI mask

---
+++ code/xenium_imageProcessing/transform.m
@@ -0,0 +1,25 @@
+Md = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC';
+img = imread(fullfile(Md,'/processed-data/xenium_imageProcessing/Br6538_HE.png'));
+
+% TrakEM2 affine transformation parameters
+a = 0.9999743970151198;
+b = -0.007155788862699962;
+c = 0.007155788862699962;
+d = 0.9999743970151198;
+e = 1816.6183235292347;
+f = 1543.900200597453;
+
+T = affine2d([a, b, 0;
+              c, d, 0;
+              e, f, 1]);
+
+% Target dimensions from srcrect_
+target_height = 11557;
+target_width = 23948;
+Rout = imref2d([target_height, target_width]);
+
+% Warp with black padding
+img_registered = imwarp(img, T, 'OutputView', Rout);
+
+% Show result
+imshow(img_registered)
