66b04e68f0764c0127a29abbbebdb1d351f7f0c9
Madhavi Tippani <madhavitippani28@gmail.com>
2025-06-23 12:33:13 -0400

    thicken cell boundary

--- code/xenium_imageProcessing/overlay.m
+++ code/xenium_imageProcessing/overlay.m
@@ -4,4 +4,18 @@ load(fullfile(Md, od, 'cell.mat'))
 scale = 0.84;
 cellmaskL = imresize(cellmask, scale, 'nearest');
 
-img = imread(fullfile(Md,od,'Br6538_HE_aligned.png'));
+img_registered = imread(fullfile(Md,od,'Br6538_HE_aligned.png'));
+img = im2double(img_registered);
+
+% Generate boundary mask
+boundary_mask = bwperim(cellmaskL > 0);
+thick_boundary = imdilate(boundary_mask, strel('disk', 2));
+
+img(repmat(thick_boundary, [1 1 3])) = 0;
+
+% Overlay red boundaries
+img(:,:,1) = img(:,:,1) + boundary_mask;  % Add red
+img(:,:,2) = img(:,:,2) .* ~boundary_mask; % Suppress green
+img(:,:,3) = img(:,:,3) .* ~boundary_mask; % Suppress blue
+
+img_overlay = min(img, 1);
