7b3845b05daa91f2a45a70d3a30f4877990d6a4c
Madhavi Tippani <madhavitippani28@gmail.com>
2025-06-25 10:58:56 -0400

    manual coord extraction for cropping HE

--- code/xenium_imageProcessing/NMseg.m
+++ code/xenium_imageProcessing/NMseg.m
@@ -3,15 +3,18 @@ fname = '/raw-data/xenium/post-xenium_images/LC_PostXenium_0068654_Slide4_40x_05
 img = imread(fullfile(Md,fname));
 img_rotated = imrotate(img, 90); 
 img_rotated = img_rotated(5000:end-8000,8000:end-5000,:);
-imshow(img_rotated(78708:92664,18004:45240,:))
 
+%%Br6538
+imshow(img_rotated(78708:92664,18004:45240,:))
 he_crop = img_rotated(78708:90000,19000:42000,:);
 save(fullfile(Md,'/processed-data/xenium_imageProcessing/Br6538_HE.mat'),'he_crop')
 imwrite(he_crop,fullfile(Md,'/processed-data/xenium_imageProcessing/Br6538_HE.png'))
 
 %further crop sample to fit into DAPI mask 
-
 img = imread(fullfile(Md,'/processed-data/xenium_imageProcessing/Br6538_HE.png'));
 he_crop = img(300:end-1100,1300:end-300,:);
 save(fullfile(Md,'/processed-data/xenium_imageProcessing/Br6538_HE.mat'),'he_crop')
 imwrite(he_crop,fullfile(Md,'/processed-data/xenium_imageProcessing/Br6538_HE.png'))
+
+
+%%Br6297
