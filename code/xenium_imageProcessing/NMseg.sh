280d7ca7216857839de7d41d253d32f01a6add38
Madhavi Tippani <madhavitippani28@gmail.com>
2025-06-30 14:07:54 -0400

    NMseg on post xenium HE image

---
+++ code/xenium_imageProcessing/NMseg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=NMseg_xenium
+#SBATCH --mem=80G
+#SBATCH -o /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/xenium_imageProcessing/logs/NMseg_Br6538.txt
+#SBATCH -e /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/xenium_imageProcessing/logs/NMseg_Br6538.txt
+
+echo "**** Job starts ****"
+date
+
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## load MATLAB
+module load matlab
+
+matlab -nodesktop -nosplash -r "NMseg"
+
+echo "**** Job ends ****"
+date
