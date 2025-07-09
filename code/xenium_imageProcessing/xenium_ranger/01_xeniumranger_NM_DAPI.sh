8fd071a91cddb661f1d85d93ce4f40538836d346
Madhavi Tippani <madhavitippani28@gmail.com>
2025-07-09 13:05:47 -0400

    specify original path to xenium bundle instead of softlink

--- code/xenium_imageProcessing/xenium_ranger/01_xeniumranger_NM_DAPI.sh
+++ code/xenium_imageProcessing/xenium_ranger/01_xeniumranger_NM_DAPI.sh
@@ -21,7 +21,8 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 module --ignore_cache load xeniumranger/3.1.1
 
 # get current sample to resegment
-FILE=/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/raw-data/xenium/output-XETG00558__0068654__Br6297__20250501__172909
+#FILE=/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/raw-data/xenium/output-XETG00558__0068654__Br6297__20250501__172909
+FILE=/dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/2025-05-05_xenium-SKCCC/output-XETG00558__0068654__Br6297__20250501__172909
 MASK=/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/processed-data/xenium_imageProcessing/Br6297/combined_nucmask.npy
 OUTPATH=xeniumranger_NM_DAPI
 
