f07e90d89c468e9bc733b5c04c4c1aaa068a2684
Madhavi Tippani <madhavitippani28@gmail.com>
2025-08-21 13:37:39 -0400

    make output directories to save split images if it doesnt exixst

--- code/xenium_imageProcessing/split_postXenium_HE.py
+++ code/xenium_imageProcessing/split_postXenium_HE.py
@@ -10,3 +10,5 @@ from pyhere import here
 pro_dir = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/'
 fname = here('raw-data/xenium/post-xenium_images/LC_PostXenium_0068968_Slide5_40x_05_13_2025_HRD.tif')
 outdir = here('processed-data/xenium_imageProcessing/split_samples/0068968_Slide5/')
+
+os.makedirs(outdir, exist_ok=True)
