9cda03d664f5ecd185f8614ed49291d67fa7f633
Madhavi Tippani <madhavitippani28@gmail.com>
2025-08-21 14:01:38 -0400

    edit 01_split_postXenium_HE.py to accept slurm variables and process

--- code/xenium_imageProcessing/01_split_postXenium_HE.py
+++ code/xenium_imageProcessing/01_split_postXenium_HE.py
@@ -1,4 +1,4 @@
-import os
+import sys,os
 os.chdir('/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/')
 import warnings
 import numpy as np
@@ -7,11 +7,19 @@ import cv2
 from skimage import morphology, measure
 from pyhere import here
 
-pro_dir = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/'
-fname = here('raw-data/xenium/post-xenium_images/LC_PostXenium_0068968_Slide5_40x_05_13_2025_HRD.tif')
-outdir = here('processed-data/xenium_imageProcessing/split_samples/0068968_Slide5/')
+slide_dir = here("raw-data/xenium/post-xenium_images")
+fname_arg = sys.argv[1]
+fname = os.path.join(slide_dir, fname_arg)
 
+# derive output directory based on sample ID
+basename = os.path.basename(fname)   # e.g. LC_PostXenium_0068968_Slide5_40x_05_13_2025_HRD.tif
+parts = basename.split("_")
+sample_id = "_".join(parts[2:4])     # "0068968_Slide5"
+outdir = here("processed-data/xenium_imageProcessing/split_samples", sample_id)
 os.makedirs(outdir, exist_ok=True)
+
+print("Input file :", fname)
+print("Output dir :", outdir)
 
 # Optional tuning knobs:
 PREVIEW_MAXDIM = 4000     # preview max dimension (pixels)
