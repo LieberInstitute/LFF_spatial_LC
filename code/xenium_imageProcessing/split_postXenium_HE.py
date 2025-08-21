d47afe212f0aece59abf76b58ff96e187a1b53a5
Madhavi Tippani <madhavitippani28@gmail.com>
2025-08-21 13:36:56 -0400

    define input output directories

--- code/xenium_imageProcessing/split_postXenium_HE.py
+++ code/xenium_imageProcessing/split_postXenium_HE.py
@@ -6,3 +6,7 @@ import tifffile as tiff
 import cv2
 from skimage import morphology, measure
 from pyhere import here
+
+pro_dir = '/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/'
+fname = here('raw-data/xenium/post-xenium_images/LC_PostXenium_0068968_Slide5_40x_05_13_2025_HRD.tif')
+outdir = here('processed-data/xenium_imageProcessing/split_samples/0068968_Slide5/')
