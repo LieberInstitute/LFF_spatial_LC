c3381ffc8406b29df81cf750bca0fbaa2c1de764
Madhavi Tippani <madhavitippani28@gmail.com>
2025-10-09 09:00:23 -0400

    change colors of panel C to match panel F

--- code/VistoSeg/code/2a_NMsegHist.m
+++ code/VistoSeg/code/2a_NMsegHist.m
@@ -1,35 +1,40 @@
 % Create a figure
 addpath(genpath('/dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/code/VistoSeg'))
 cd /dcs05/lieber/marmaypag/LFF_spatialLC_LIBD4140/LFF_spatial_LC/
+load(fullfile(pwd,'code','VistoSeg','code','colorpalette.mat'))
 O = fullfile(pwd, 'processed-data/Images/NMseg/');
 D = fullfile(pwd,'/raw-data/Images/'); 
 myfiles = dir(fullfile(O,'*1.mat'));
  figure;
 
-for i = 1: numel(myfiles)
+parfor i = 1: numel(myfiles)
 
 disp(myfiles(i).name);
 
-   load(fullfile(myfiles(i).folder, myfiles(i).name))
-   img = rgb2gray(NM);
+   IMG = load(fullfile(myfiles(i).folder, myfiles(i).name))
+   img = rgb2gray(IMG.NM);
    img = mat2gray(img);
    
     hold on;
-    histogram(img(:), 'Normalization', 'probability', 'EdgeColor', 'none', ...
-        'FaceColor', rand(1,3));  % Random color for each histogram
-      if mod(i, 15) == 0
+    histogram(img(:), 'Normalization', 'probability', 'EdgeColor', 'none', 'FaceColor', C(i,:));  % Random color for each histogram
+       % 'FaceColor', rand(1,3));  % Random color for each histogram
+      if mod(i, 5) == 0
         % Save the figure with a unique name
-        saveas(gcf, fullfile(pwd, 'plots', 'NMseg', sprintf('NMseghist_%d.png', i)));
         xlim([0,0.6])
         ylim([0,0.005])
+		xlabel('Pixel Intensity');
+		ylabel('Probability');
+		xline(0.2,'--r')
+		xline(0.5,'--r')
+		saveas(gcf, fullfile(pwd, 'plots', 'NMseg', sprintf('NMseghist_%d.png', i)));
       end
 end
 
 % Add labels and title
-xlabel('Pixel Intensity');
+
 %xlim([0,0.6])
 %ylim([0,0.005])
-ylabel('Probability');
+
 title('Histograms of Images');
 hold off;
 
